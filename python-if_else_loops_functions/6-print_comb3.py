71cff0be741a69c683a5716730aec506d8b85847
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-17 14:00:48 +0100

    put print 89

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-for firstnumber in range(0, 10):
-    for secondnumber in range(0, 10):
+for firstnumber in range(0, 8):
+    for secondnumber in range(1, 10):
         if (firstnumber != secondnumber and firstnumber < secondnumber):
             print("{}".format(firstnumber) + "{}".format(secondnumber), end=", ")
+print(89)
+
+
