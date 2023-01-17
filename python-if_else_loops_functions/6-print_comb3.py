4c6f0280a535f4958ed85cea267189af44f1d21a
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-17 14:03:09 +0100

    change variable name

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-for firstnumber in range(0, 8):
-    for secondnumber in range(1, 10):
-        if (firstnumber != secondnumber and firstnumber < secondnumber):
-            print("{}".format(firstnumber) + "{}".format(secondnumber), end=", ")
+for number1 in range(0, 8):
+    for number2 in range(1, 10):
+        if (number1 != number2 and number1 < number2):
+            print("{}".format(number1) + "{}".format(number2), end=", ")
 print(89)
-
-
