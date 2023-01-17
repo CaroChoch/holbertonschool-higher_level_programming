d2f5af5a31be6751cc9d056343f0cbb04920c1e0
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-17 13:37:41 +0100

    remove str and use format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@
 for firstnumber in range(0, 10):
     for secondnumber in range(0, 10):
         if (firstnumber != secondnumber and firstnumber < secondnumber):
-            print(str(firstnumber) + str(secondnumber), end=", ")
+            print("{}".format(firstnumber) + "{}".format(secondnumber), end=", ")
