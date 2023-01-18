0036838b68e8b421c9ca55e81753ad990ac4fc5b
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-18 15:52:25 +0100

    fix code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from sys import argv
+    import sys
 
-    argv_count = len(argv) - 1
+    argv_count = len(sys.argv) - 1
 
     if argv_count == 0:
         print("{:d} arguments.".format(argv_count))
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         print("{:d} argument:".format(argv_count))
     else:
         print("{:d} arguments.".format(argv_count))
-    for index in range(1, argv_count + 1):
-        print("{:d}: {:s}".format(index, sys.argv[index]))
+    for index in range(argv_count):
+        print("{:d}: {:s}".format(index + 1, sys.argv[index + 1]))
