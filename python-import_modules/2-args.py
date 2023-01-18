173861ec121712277eb6714881e4d3ff88b5f650
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-18 15:55:20 +0100

    empty braces

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     argv_count = len(sys.argv) - 1
 
     if argv_count == 0:
-        print("{:d} arguments.".format(argv_count))
+        print("{} arguments.".format(argv_count))
     elif argv_count == 1:
-        print("{:d} argument:".format(argv_count))
+        print("{} argument:".format(argv_count))
     else:
-        print("{:d} arguments.".format(argv_count))
+        print("{} arguments.".format(argv_count))
     for index in range(argv_count):
-        print("{:d}: {:s}".format(index + 1, sys.argv[index + 1]))
+        print("{}: {}".format(index + 1, sys.argv[index + 1]))
