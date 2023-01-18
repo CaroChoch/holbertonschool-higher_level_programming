b3b3a97975b3354b3422d14a05ed89f37ba2f8eb
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-18 15:41:44 +0100

    add a else statement

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,11 @@ if __name__ == "__main__":
 
     argv_count = len(sys.argv) - 1
 
-    if argv_count == 0 or argv_count > 1:
+    if argv_count == 0:
         print("{:d} arguments.".format(argv_count))
     elif argv_count == 1:
         print("{:d} argument:".format(argv_count))
+    else:
+        print("{:d} arguments.".format(argv_count))
     for index in range(1, argv_count + 1):
         print("{:d}: {:s}".format(index, sys.argv[index]))
