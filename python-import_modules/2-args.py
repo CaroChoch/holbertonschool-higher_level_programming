67e2c076abc97193b1e203446702f4c71ab75f56
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-18 16:35:35 +0100

    typo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 == 0:
-        print("{} arguments.".format(len(sys.argv) - 1),)
+        print("{} arguments.".format(len(sys.argv) - 1))
     else:
         if len(sys.argv) - 1 == 1:
             print("{} argument:".format(len(sys.argv) - 1))
