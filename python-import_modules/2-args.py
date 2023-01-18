b4774635a1350fac15f9f99b8748dcc69f1db879
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-18 16:30:20 +0100

    typo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,12 +4,11 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 == 0:
-        print("{} arguments.".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1),)
     else:
         if len(sys.argv) - 1 == 1:
             print("{} argument:".format(len(sys.argv) - 1))
         else:
-            print("{} arguments.".format(len(sys.argv) - 1))
-    if len(sys.argv) != 0:
-        for index in range(1, len(sys.argv)):
-            print("{}: {}".format(index, sys.argv[index]))
+            print("{} arguments:".format(len(sys.argv) - 1))
+    for index in range(1, len(sys.argv)):
+        print("{}: {}".format(index, sys.argv[index]))
