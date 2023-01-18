55b45a2e43544cb81eb5f1ce82ce597a48547134
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-18 16:15:05 +0100

    add a statement before the loop

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
             print("{} argument:".format(len(sys.argv) - 1))
         else:
             print("{} arguments.".format(len(sys.argv) - 1))
+    if len(sys.argv) != 0:
         for index in range(1, len(sys.argv)):
             print("{}: {}".format(index, sys.argv[index]))
