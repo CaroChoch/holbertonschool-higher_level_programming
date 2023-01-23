89d7bd17eb585e78cda89bd1d8bd2a4b59272f93
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-23 12:08:51 +0100

    PEP8 validation

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-  for row in matrix:
+    for row in matrix:
         for value in range(len(row)):
             print("{:d}".format(row[value]), end="")
             if value < len(row) - 1:
