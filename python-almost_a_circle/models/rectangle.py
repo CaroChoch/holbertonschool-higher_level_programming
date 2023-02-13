a46dd9d990220e1119e3cd253bb242f9eef5b681
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-13 12:48:17 +0100

    first rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
-
+    """
+    the class Rectangle inherits from Base
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
