8afbe4c86543e6b949834b68bb1fb2f4512a820c
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-13 20:16:18 +0100

    Dictionary to instance

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -50,3 +50,14 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set """
+        if cls.__name__ == "Rectangle":
+            instance = cls(1, 4)
+        else:
+            instance = cls(1)
+
+        instance.update(**dictionary)
+        return instance
