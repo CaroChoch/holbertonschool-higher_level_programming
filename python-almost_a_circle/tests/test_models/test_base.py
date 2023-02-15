03e1c224d8a5eca4898e48d45bf526fe9428095c
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-02-15 15:40:15 +0100

    second trial of tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -9,6 +9,10 @@ class TestBase(unittest.TestCase):
         self.assertEqual(Base().id, 1)
         self.assertEqual(Base().id, 2)
         self.assertEqual(Base(89).id, 89)
+        self.assertEqual(Base('a').id, 'a')
+        #self.assertEqual(Base("abc".id, "abc"))
+        #self.assertEqual(Base(None).id, None)
+        
 
 if __name__ == '__main__':
     unittest.main()
