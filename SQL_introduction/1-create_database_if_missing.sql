68be704c3ad428ce4cc04a9714b90551cc24a72a
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-03-13 14:20:17 +0100

    script that creates the database hbtn_0c_0 in my MySQL server

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -2,6 +2,4 @@
 -- If the database hbtn_0c_0 already exists, the script should not fail
 -- not allowed to use the SELECT or SHOW statements
 
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0
-[CHARACTER SET charset_name]
-[COLLATE collation_name]
+CREATE DATABASE hbtn_0c_0
